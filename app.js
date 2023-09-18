f7b03a2887b4139075db896ead333fef638f213a
Jake Westerfield <jecobwesterfield@gmail.com>
2023-09-18 19:57:09 -0400

    adds in app.js data. the home page which renders the index view. And the /generate endpoint which will generate a specific URL or req.query.data object else error

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ejs = require('ejs');
+const QRCode = require('qrcode');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/generate', (req, res) => {
+    const data = req.query.data || 'https://example.com';
+    // res.render();
+
+    QRCode.toDataURL(data, (err, url) => {
+        if (err) {
+            console.error(err);
+            res.send('An error occured.');
+        } else {
+            res.render('index', { qrDataURL: url });
+        }
+    });
+});
